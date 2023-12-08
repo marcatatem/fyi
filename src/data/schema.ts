39e6a682470ab7539a29297c582cb950d05e1d77
Marca Tatem <marca@me.com>
2023-12-07 18:10:19 -0800

    updating jsonld

--- src/data/schema.ts
+++ src/data/schema.ts
@@ -2,33 +2,26 @@ import meta from "data/meta.json" with { type: "json" };
 
 export const schema = {
   "@context": "https://schema.org",
-  "@type": "ProfilePage",
-  "@id": "ProfilePage",
-  "about": {
-    "@type": "Person",
-    "givenName": "Marca",
-    "familyName": "Tatem",
-    "address": {
-      "@type": "PostalAddress",
-      "addressLocality": "San Francisco",
-      "addressRegion": "CA",
+  "@type": "Person",
+  "givenName": "Marca",
+  "familyName": "Tatem",
+  "address": {
+    "@type": "PostalAddress",
+    "addressLocality": "San Francisco",
+    "addressRegion": "CA",
+  },
+  "knowsLanguage": [
+    {
+      "@type": "Language",
+      "name": "English",
     },
-    "knowsLanguage": [
-      {
-        "@type": "Language",
-        "name": "English",
-      },
-      {
-        "@type": "Language",
-        "name": "French",
-      },
-    ],
-  },
-  "abstract": meta.head.description,
-  "mainContentOfPage": {
-    "cssSelector": "article",
-  },
-  "primaryImageOfPage": {
+    {
+      "@type": "Language",
+      "name": "French",
+    },
+  ],
+  "description": meta.head.description,
+  "image": {
     "@type": "ImageObject",
     "url": "https://marca.fyi/img/marca.jpg",
     "caption": "Portrait of Marca",
